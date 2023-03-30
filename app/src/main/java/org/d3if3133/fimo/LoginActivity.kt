6a157bbef6bc6245468d9e79078b1f5a48b11cc2
alfinabalqis <alfinabalqis28@gmail.com>
2023-03-31 06:42:26 +0700

    add logic at main activity, add kategori item and error formatt currency

--- app/src/main/java/org/d3if3133/fimo/LoginActivity.kt
+++ app/src/main/java/org/d3if3133/fimo/LoginActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import org.d3if3133.fimo.databinding.ActivityLoginBinding
-
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
@@ -15,8 +14,59 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.btnLogin.setOnClickListener{ loginButton()}
+
+//        binding.editSaldo.addTextChangedListener(object : TextWatcher{
+//            private var current = ""
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//
+//            override fun afterTextChanged(s: Editable?) {
+//                if(s.toString() != current){
+//                    binding.editSaldo.removeTextChangedListener(this)
+//
+//                    val cleanString = s.toString().replace("[Rp ,.\\s]".toRegex(), "")
+//                    val parsed = cleanString.toLong()
+//                    val formatted = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(parsed)
+//
+//                    current = formatted
+//                    binding.editSaldo.setText(formatted)
+//                    binding.editSaldo.setSelection(formatted.length)
+//                    binding.editSaldo.addTextChangedListener(this)
+//                }
+//            }
+//        })
+
+
+        // val editSaldo = findViewById<TextInputEditText>(R.id.editSaldo)
+        //
+        //        // Set locale ke Indonesia untuk format mata uang rupiah
+        //        val localeID = Locale("in", "ID")
+        //
+        //        // Buat objek NumberFormat untuk format mata uang rupiah
+        //        val formatRupiah = NumberFormat.getCurrencyInstance(localeID)
+        //
+        //        editSaldo.addTextChangedListener(object : TextWatcher {
+        //            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        //
+        //            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        //
+        //            override fun afterTextChanged(s: Editable?) {
+        //                if (s.toString().isNotEmpty()) {
+        //                    // Konversi input ke dalam bentuk angka
+        //                    val saldo = s.toString().toLong()
+        //
+        //                    // Format angka menjadi format mata uang rupiah
+        //                    val saldoFormatted = formatRupiah.format(saldo)
+        //
+        //                    // Set text pada TextInputEditText dengan format mata uang rupiah
+        //                    editSaldo.removeTextChangedListener(this)
+        //                    editSaldo.setText(saldoFormatted)
+        //                    editSaldo.setSelection(saldoFormatted.length)
+        //                    editSaldo.addTextChangedListener(this)
+        //                }
     }
 
     private fun loginButton() {
