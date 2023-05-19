cbdd68b7984d044df5b88a8409127eb509a20af5
alfinabalqis <alfinabalqis28@gmail.com>
2023-05-19 19:57:15 +0700

    fix error and finish yeyyyy

--- app/src/main/java/org/d3if3133/fimo/db/FimoApplication.kt
+++
@@ -1,9 +0,0 @@
-package org.d3if3133.fimo.db
-
-import android.app.Application
-
-class FimoApplication : Application() {
-    val database: ItemRoomDatabase by lazy {
-        ItemRoomDatabase.getDatabase(this)
-    }
-}
--- app/src/main/java/org/d3if3133/fimo/ui/AboutFragment.kt
+++
@@ -1,6 +0,0 @@
-package org.d3if3133.fimo.ui
-
-import androidx.fragment.app.Fragment
-import org.d3if3133.fimo.R
-
-class AboutFragment : Fragment(R.layout.fragment_about)
