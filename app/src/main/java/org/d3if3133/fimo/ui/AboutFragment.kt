63216b6013c17fae0f8ee43e398dc3d6b77bf774
alfinabalqis <alfinabalqis28@gmail.com>
2023-05-19 10:44:03 +0700

    add login and about fragment

--- app/src/main/java/org/d3if3133/fimo/ui/AboutFragment.kt
+++ app/src/main/java/org/d3if3133/fimo/ui/AboutFragment.kt
@@ -1,5 +1,6 @@
-package org.d3if3133.fimo
+package org.d3if3133.fimo.ui
 
 import androidx.fragment.app.Fragment
+import org.d3if3133.fimo.R
 
 class AboutFragment : Fragment(R.layout.fragment_about)
